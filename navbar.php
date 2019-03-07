9a7cb785bb5ef6bcb3646b37a8dc1894b33bfcf9
Wendyhartford <33272230+Wendyhartford@users.noreply.github.com>
2019-03-07 09:27:19 -0700

    Create navbar.php
    php navbar file

---
+++ navbar.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+<link rel = "stylesheet"
+   type = "text/css"
+   href = "stylesheet.css" />
+	<link href="https://fonts.googleapis.com/css?family=Lobster" rel="stylesheet">
+
+	
+	
+	<nav class="topnav" id = "menu">
+  	<li><a class="active" href="#home">Home</a></li>
+  	<li><a href="#hartford">Wendy Hartford</a></li>
+  	<li><a href="#keoloha">Brandon Keoloha</a></li>
+  	<li><a href="#sacino">Joey Sacino</a></li>
+	<li><a href="#wirick">Rebecca Wirick</a></li>
+</nav>
+</head>
+</html>
