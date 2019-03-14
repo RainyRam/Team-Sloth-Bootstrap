30930da00da6520460138ae35e497feb3d12d54c
Wendyhartford <33272230+Wendyhartford@users.noreply.github.com>
2019-03-14 08:54:31 -0600

    Adding Bootstrap

--- navbar.php
+++ navbar.php
@@ -1,20 +1,8 @@
-<!DOCTYPE html>
-<html>
-<head>
-
-<link rel = "stylesheet-nav"
-   type = "text/css"
-   href = "stylesheet-nav.css" />
-	<link href="https://fonts.googleapis.com/css?family=Lobster" rel="stylesheet">
-
-	
-	
-	<nav class="topnav" id = "menu">
+<?php echo '
   	<li><a class="active" href="#home">Home</a></li>
-  	<li><a href="#hartford">Wendy Hartford</a></li>
-  	<li><a href="#keoloha">Brandon Keoloha</a></li>
-  	<li><a href="#sacino">Joey Sacino</a></li>
-	<li><a href="#wirick">Rebecca Wirick</a></li>
-</nav>
-</head>
-</html>
+  	<li><a href="wendyPage.html">Wendy Hartford</a></li>
+  	<li><a href="brandonPage.html">Brandon Keoloha</a></li>
+  	<li><a href="joeyPage.html">Joey Sacino</a></li>
+	<li><a href="rebeccaPage.html">Rebecca Wirick</a></li>
+
+';?>
